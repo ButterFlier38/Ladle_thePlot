af45a98d869fbac49d7fc7165023987d37ada99c
JasmineAufiero <jasmineaufiero@gmail.com>
2022-02-11 10:56:23 +0100

    Adding the model: Avatars and Recipes

---
+++ Ladle_thePlot/Model/Recipe.swift
@@ -0,0 +1,22 @@
+//
+//  Recipe.swift
+//  Ladle_thePlot
+//
+//  Created by Jasmine Aufiero on 11/02/22.
+//
+
+import Foundation
+class Recipe : Identifiable {
+    let id = UUID()
+    var name :String // name of the recipe
+    var ingredients :[String: String] // all the ingredients (A dictionary in which the key is the incredient of the recipe, and the value is the quantity)
+    var steps : [Int: String] // all the steps (the key is the step number and the value the passages related to that number)
+    var isChoosen :Bool
+    
+    internal init(name: String, ingredients :[String :String], steps :[Int: String], isChoosen: Bool) {
+        self.name = name
+        self.ingredients = ingredients
+        self.steps = steps
+        self.isChoosen = isChoosen
+    }
+}
