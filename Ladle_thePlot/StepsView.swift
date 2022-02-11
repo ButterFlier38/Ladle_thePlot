581be36f5b44e0e8f44eba36a489def5e12664b7
Irene Fernando <irene87fernando@gmail.com>
2022-02-11 10:00:56 +0100

    All the Views added

--- Ladle_thePlot/StepsView.swift
+++ Ladle_thePlot/StepsView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct StepsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Helo, World!")
     }
 }
 
