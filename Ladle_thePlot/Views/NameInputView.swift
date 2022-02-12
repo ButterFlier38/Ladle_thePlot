2b69c6e10362a7ab9000a7a4de51587071ca5b75
Irene Fernando <irene87fernando@gmail.com>
2022-02-12 20:07:44 +0100

    Recreated the InputNameView with the new Design

--- Ladle_thePlot/Views/NameInputView.swift
+++ Ladle_thePlot/Views/NameInputView.swift
@@ -23,35 +23,91 @@ struct CustomColor {
 
 
 
+
 struct NameInputView: View {
     @State private var username: String = ""
     var body: some View {
         ZStack{
-            Circle().foregroundColor(CustomColor.bgyellow).scaleEffect(0.8).position(x: 550, y: 400)
-            Circle().foregroundColor(CustomColor.bgpink).scaleEffect(0.6).position(x: 250, y: 100)
-            Circle().foregroundColor(CustomColor.bggreen).scaleEffect(0.8).position(x: 1000, y: 200)
-            
-            Circle().foregroundColor(CustomColor.bgblue).scaleEffect(0.6).position(x: 600, y: 80)
-            Image("character_tonia").scaleEffect(0.03).position(x: 720, y: 100)
-            Image("character_giorgia").scaleEffect(0.03).position(x: 460, y: 100)
-            Image("character_nino").scaleEffect(0.03).position(x: 550, y: 120)
-            Image("character_gino").scaleEffect(0.03).position(x: 640, y: 125)
-           Spacer()
-            VStack {
-                Text("What's your child's name?").font(Font.custom("HappyMonkey-Regular", size: 30 )).fontWeight(.bold).foregroundColor(CustomColor.selectionblue) .shadow(color: CustomColor.selectionblue, radius: 1)
-            
-        
+            Circle().foregroundColor(CustomColor.bgyellow).scaleEffect(0.7).position(x: 1050, y: 700)
+            Circle().foregroundColor(CustomColor.bgpink).scaleEffect(0.7).position(x: 150, y: 100)
+            Circle().foregroundColor(CustomColor.bggreen).scaleEffect(0.7).position(x: 150, y: 700)
+
+            Circle().foregroundColor(CustomColor.bgblue).scaleEffect(0.7).position(x: 1050, y: 100)
+            VStack{
+            ZStack{
+                RoundedRectangle(cornerRadius: 80, style: .continuous)
+                                .fill(Color.white)
+                                .frame(width: 600, height: 320, alignment: .center)
+                                .overlay(Image("character_tonia").scaleEffect(0.03).position(x: 430, y: 120))
+                                .overlay(Image("character_giorgia").scaleEffect(0.03).position(x: 160, y: 120))
+                                .overlay(Image("character_nino").scaleEffect(0.03).position(x: 250, y: 120))
+                                .overlay(Image("character_gino").scaleEffect(0.03).position(x: 340, y: 120))
+                                .overlay( Text("What's your child's name?").font(Font.custom("HappyMonkey-Regular", size: 35 )).fontWeight(.bold).foregroundColor(CustomColor.selectionblue) .shadow(color: CustomColor.selectionblue, radius: 1).position(x: 300, y: 250))
+           }
+           
                 ZStack{
-                    RoundedRectangle(cornerRadius: 30, style: .continuous)
+                    RoundedRectangle(cornerRadius: 60, style: .continuous)
                                     .fill(Color.white)
-                                    .frame(width: 300, height: 80, alignment: .center)
-                    
+                                    .frame(width: 500, height: 60, alignment: .center)
+                                    .overlay(
+                                         RoundedRectangle(cornerRadius: 60)
+                                             .stroke(CustomColor.selectionblue, lineWidth: 4))
+
                     TextField("Input name", text: $username).frame(width: 300, height: 80, alignment: .center).font(Font.custom("HappyMonkey-Regular", size: 20)).foregroundColor(CustomColor.selectionblue) .shadow(color: CustomColor.bgblue, radius: 1)
                    }
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    RoundedRectangle(cornerRadius: 60, style: .continuous)
+                                    .fill(CustomColor.selectionblue)
+                                    .frame(width: 200, height: 60, alignment: .center)
+                                    .overlay(
+                                        Text("Continue").font(Font.custom("HappyMonkey-Regular", size: 25 )).foregroundColor(.white).shadow(color: .white, radius: 1))                }
+               
+                
+            }
             }
         }
     }
-}
+
+
+
+
+
+
+
+
+//****** PREVIOUS DESIGN *****
+
+//struct NameInputView: View {
+//    @State private var username: String = ""
+//    var body: some View {
+//        ZStack{
+//            Circle().foregroundColor(CustomColor.bgyellow).scaleEffect(0.8).position(x: 550, y: 400)
+//            Circle().foregroundColor(CustomColor.bgpink).scaleEffect(0.6).position(x: 250, y: 100)
+//            Circle().foregroundColor(CustomColor.bggreen).scaleEffect(0.8).position(x: 1000, y: 200)
+//
+//            Circle().foregroundColor(CustomColor.bgblue).scaleEffect(0.6).position(x: 600, y: 80)
+//            Image("character_tonia").scaleEffect(0.03).position(x: 720, y: 100)
+//            Image("character_giorgia").scaleEffect(0.03).position(x: 460, y: 100)
+//            Image("character_nino").scaleEffect(0.03).position(x: 550, y: 120)
+//            Image("character_gino").scaleEffect(0.03).position(x: 640, y: 125)
+//           Spacer()
+//            VStack {
+//                Text("What's your child's name?").font(Font.custom("HappyMonkey-Regular", size: 30 )).fontWeight(.bold).foregroundColor(CustomColor.selectionblue) .shadow(color: CustomColor.selectionblue, radius: 1)
+//
+//
+//                ZStack{
+//                    RoundedRectangle(cornerRadius: 30, style: .continuous)
+//                                    .fill(Color.white)
+//                                    .frame(width: 300, height: 80, alignment: .center)
+//
+//                    TextField("Input name", text: $username).frame(width: 300, height: 80, alignment: .center).font(Font.custom("HappyMonkey-Regular", size: 20)).foregroundColor(CustomColor.selectionblue) .shadow(color: CustomColor.bgblue, radius: 1)
+//                   }
+//            }
+//        }
+//    }
+//}
+//
+//  *********
 
 struct NameInputView_Previews: PreviewProvider {
     static var previews: some View {
