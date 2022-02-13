eb9326beccef1469822d152c4e751af825e3f47c
Irene Fernando <irene87fernando@gmail.com>
2022-02-13 16:17:45 +0100

    Button Highlights completed in AvatarSelector

--- Ladle_thePlot/Views/AvatarSelectorView.swift
+++ Ladle_thePlot/Views/AvatarSelectorView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct AvatarSelectorView: View {
 
     @State private var username: String = ""
+    @State private var Giorgia: Bool = false
+    @State private var Tonia: Bool = false
+    @State private var Gino: Bool = false
+    @State private var Nino: Bool = false
     var body: some View {
         ZStack{
             Circle().foregroundColor(CustomColor.bgyellow).scaleEffect(0.7).position(x: 1050, y: 700)
@@ -31,46 +35,68 @@ struct AvatarSelectorView: View {
                 ZStack{
                     RoundedRectangle(cornerRadius: 80, style: .continuous)
                                     .fill(Color.white)
-                                    .frame(width: 900, height: 300, alignment: .center)
-                HStack(alignment: .center, spacing: 20){
+                                    .frame(width: 1000, height: 300, alignment: .center)
+                HStack(alignment: .center, spacing: 30){
 
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                        Circle().foregroundColor(.white)
+                    Button(action:  {withAnimation(.easeOut(duration: 0.2)) {
+                        self.Giorgia.toggle()
+                        self.Tonia = false
+                        self.Gino = false
+                        self.Nino = false
+                    }}) {
+                        Circle().scaleEffect(self.Giorgia ? 1.2 : 1).foregroundColor(self.Giorgia ? CustomColor.selectiongreen : Color.white)
                             .overlay(Circle().stroke(CustomColor.selectiongreen, lineWidth: 4)
                             .overlay(Image("character_giorgia")
-                                .scaleEffect(0.035)
+                                        .scaleEffect( self.Giorgia ? 0.05 : 0.04)
                                         .padding(.trailing)
-                                 .clipShape(Circle().scale(0.047))
-                            ))
-                      }
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                        Circle().foregroundColor(.white)
+                                 .clipShape(Circle().scale( self.Giorgia ? 0.07 : 0.055))
+                            ))}
+                    
+//                    (configuration.isPressed ? Color.red : Color.yellow)
+//                    if self.toggleView == item { // << selection !!
+//                                               HiddenDescriptionView()
+//                                           }
+                    
+                    Button(action:  {withAnimation(.easeOut(duration: 0.2)) {
+                        self.Giorgia = false
+                        self.Tonia = false
+                        self.Gino.toggle()
+                        self.Nino = false
+                    }}) {
+                        Circle().scaleEffect(self.Gino ? 1.2 : 1).foregroundColor(self.Gino ? CustomColor.selectionpink : Color.white)
                             .overlay(Circle().stroke(CustomColor.selectionpink, lineWidth: 4)
                             .overlay(Image("character_gino")
-                                .scaleEffect(0.033)
-                                        .padding(.top)
-                                 .clipShape(Circle().scale(0.047))
-                            ))
-                      }
-              
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                        Circle().foregroundColor(.white)
+                                        .scaleEffect( self.Gino ? 0.05 : 0.04)
+                                        .padding(.trailing)
+                                 .clipShape(Circle().scale( self.Gino ? 0.069 : 0.055))
+                            ))}
+               
+                    Button(action:  {withAnimation(.easeOut(duration: 0.2)) {
+                        self.Tonia.toggle()
+                        self.Giorgia = false
+                        self.Gino = false
+                        self.Nino = false
+                    }}) {
+                        Circle().scaleEffect(self.Tonia ? 1.2 : 1).foregroundColor(self.Tonia ? CustomColor.selectionblue : Color.white)
                             .overlay(Circle().stroke(CustomColor.selectionblue, lineWidth: 4)
                             .overlay(Image("character_tonia")
-                                .scaleEffect(0.035)
-                                        .padding(.top)
-                                 .clipShape(Circle().scale(0.038))
-                            ))
-                      }
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                    Circle().foregroundColor(.white)
-                        .overlay(Circle().stroke(CustomColor.selectionyellow, lineWidth: 4)
-                        .overlay(Image("character_nino")
-                            .scaleEffect(0.033)
-                                    .padding(.top)
-                             .clipShape(Circle().scale(0.044))
-                        ))
-                  }
+                                        .scaleEffect( self.Tonia ? 0.05 : 0.04)
+                                        .padding(.trailing)
+                                 .clipShape(Circle().scale( self.Tonia ? 0.055 : 0.046))
+                            ))}
+                    Button(action:  {withAnimation(.easeOut(duration: 0.2)) {
+                        self.Giorgia = false
+                        self.Tonia = false
+                        self.Gino = false
+                        self.Nino.toggle()
+                    }}) {
+                        Circle().scaleEffect(self.Nino ? 1.2 : 1).foregroundColor(self.Nino ? CustomColor.selectionyellow : Color.white)
+                            .overlay(Circle().stroke(CustomColor.selectionyellow, lineWidth: 4)
+                            .overlay(Image("character_nino")
+                                        .scaleEffect( self.Nino ? 0.05 : 0.04)
+                                        .padding(.trailing)
+                                 .clipShape(Circle().scale( self.Nino ? 0.062 : 0.052))
+                            ))}
                     
                
                 //                Image("character_gino")
@@ -79,7 +105,7 @@ struct AvatarSelectorView: View {
                    
     //                         .overlay(Circle().stroke(Color.red, lineWidth: 5))
                     
-                }.frame(width: 800, height: 300, alignment: .center)
+                }.frame(width: 900, height: 300, alignment: .center)
             }
                 Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
                     RoundedRectangle(cornerRadius: 60, style: .continuous)
