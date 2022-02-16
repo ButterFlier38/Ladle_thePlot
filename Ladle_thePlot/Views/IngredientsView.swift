5813c5cc71c15c0d5203da54e692172c4c146296
Rosa Cimmino <rosa.cimmino.1997@gmail.com>
2022-02-16 12:08:31 +0100

    ingredients view and letsmakeitView

--- Ladle_thePlot/Views/IngredientsView.swift
+++ Ladle_thePlot/Views/IngredientsView.swift
@@ -1,3 +1,4 @@
+
 //
 //  IngredientsView.swift
 //  Ladle_thePlot
@@ -9,59 +10,127 @@ import SwiftUI
 
 struct IngredientsView: View {
     var body: some View {
-        ZStack{
-            Circle().foregroundColor(CustomColor.bgyellow).scaleEffect(0.7).position(x: 1050, y: 700)
-            Circle().foregroundColor(CustomColor.bgpink).scaleEffect(0.7).position(x: 150, y: 100)
-            Circle().foregroundColor(CustomColor.bggreen).scaleEffect(0.7).position(x: 150, y: 700)
-
-            Circle().foregroundColor(CustomColor.bgblue).scaleEffect(0.7).position(x: 1050, y: 100)
-            RoundedRectangle(cornerRadius: 80, style: .continuous)
-                            .fill(Color.white)
-                            .frame(width: 800, height: 320, alignment: .center)
-                            
-            HStack(alignment: .center, spacing: 10){
-    
-//                Image("character_gino")
-//                    .scaleEffect(0.03)
-            
-            
-           
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                    Circle().overlay(
-                    Image("character_gino")
-                             .clipShape(Circle().scale(0.05))
-                              .shadow(radius: 10)
-                        .overlay(Circle().stroke(Color.red, lineWidth: 5)) )}
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                    Circle().overlay(
-                    Image("character_gino")
-                             .clipShape(Circle().scale(0.05))
-                              .shadow(radius: 10)
-                        .overlay(Circle().stroke(Color.red, lineWidth: 5)) )}
-          
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                    Circle().overlay(
-                    Image("character_gino")
-                             .clipShape(Circle().scale(0.05))
-                              .shadow(radius: 10)
-                        .overlay(Circle().stroke(Color.red, lineWidth: 5)) )}
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                Circle().overlay(
-                    Image("character_gino").scaleEffect(0.04)
-                         .clipShape(Circle().scale(0.05))
-                          .shadow(radius: 10)
-                .overlay(Circle().stroke(Color.red, lineWidth: 5)) )
-            }
-           
-            //                Image("character_gino")
-//                        .clipShape(Circle().scale(0.05))
-//                         .shadow(radius: 10)
-               
-//                         .overlay(Circle().stroke(Color.red, lineWidth: 5))
-                
-            }.frame(width: 700, height: 250, alignment: .center)
         
-        }
+        GeometryReader { geometry in
+        ZStack{
+            Circle().foregroundColor(CustomColor.bggreen).scaleEffect(0.57).position(x: 650, y: 170)
+            Circle().foregroundColor(CustomColor.bgblue).scaleEffect(0.45).position(x: 370, y: 80)
+            Circle().foregroundColor(CustomColor.bgpink).scaleEffect(0.35).position(x: 1100, y: 60)
+            Circle().foregroundColor(CustomColor.bgyellow).scaleEffect(0.45).position(x: 900, y: 100)
+                        
+            
+            Ellipse().foregroundColor(CustomColor.bggreen)
+                .scaleEffect(1.2).position(x: 570, y: 950)
+                       
+            VStack{
+                
+                
+//                button back
+//                Spacer()
+                
+                
+                Text ("Ingredients")
+                    .font(Font.custom("HappyMonkey-Regular", size: 80 )).fontWeight(.bold).foregroundColor(CustomColor.selectionblue)  .frame(maxWidth: .infinity, alignment: .leading) .padding(.leading)
+                    
+            Spacer()
+                            
+            HStack(alignment: .center, spacing: 80){
+                
+                VStack(alignment: .center, spacing: 9){
+                ZStack{
+                    Circle()
+                        .scaleEffect(1.1)
+                        .foregroundColor(.white)
+                        .overlay(Image("latte").scaleEffect(0.02))
+                      
+                        
+                }
+                    Text("100 ml of Condensed Milk")
+                        .font(Font.custom("HappyMonkey-Regular", size:30 )).fontWeight(.bold).foregroundColor(CustomColor.selectionblue)  .frame(maxWidth: .infinity, alignment: .center) .padding(.leading)
+                    
+                }
+                
+                
+                
+            VStack (spacing: 9){
+                    ZStack{
+                Circle()
+                            .scaleEffect(1.1)
+                    .foregroundColor(.white)
+                    .overlay(Image("latte").scaleEffect(0.02))
+                    }
+                    Text("50 gr of Ice Cubes")
+                    .font(Font.custom("HappyMonkey-Regular", size: 30 )).fontWeight(.bold).foregroundColor(CustomColor.selectionblue)  .frame(maxWidth: .infinity, alignment: .center)
+                        .padding(.leading)
+                        
+                    
+            }
+                
+                VStack (spacing: 9){
+                        ZStack{
+                    Circle()
+                                .scaleEffect(1.1)
+                        .foregroundColor(.white)
+                        .overlay(Image("fragola").scaleEffect(0.02))
+                        }
+                        Text("50 gr of Strawberry")
+                        .font(Font.custom("HappyMonkey-Regular", size: 30 )).fontWeight(.bold).foregroundColor(CustomColor.selectionblue)  .frame(maxWidth: .infinity, alignment: .center)
+                            .padding(.leading)
+                    
+                }
+        
+//                sbdnkcnakvnaj
+                
+                VStack (spacing: 9){
+                        ZStack{
+                    Circle()
+                        .scaleEffect(1.1)
+                        .foregroundColor(.white)
+                        .overlay(Image("banana").scaleEffect(0.02))
+                        }
+                        Text("50 gr of Banana")
+                        .font(Font.custom("HappyMonkey-Regular", size: 30 )).fontWeight(.bold).foregroundColor(CustomColor.selectionblue)  .frame(maxWidth: .infinity, alignment: .center)
+                            .padding(.leading)
+                    
+                }
+                
+  
+            } .frame(height: geometry.size.height * 0.4,  alignment: .topLeading)
+//        close Hstack
+                
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+//                nsdvnsjdvjsdfnvjn
+
+            } //close vstack
+            .frame(height: geometry.size.height, alignment: .center)
+            
+            Image ("character_giorgia")
+                    .resizable()
+                    .scaleEffect(0.5)
+                    .aspectRatio(0.7, contentMode: .fit)
+                    .position(x: 170, y: 650)
+            
+            
+            NavigationLink {
+               LetsmakeitView()
+            } label: {
+                RoundedRectangle(cornerRadius: 60, style: .continuous)
+                    .fill(CustomColor.selectionblue)
+                    .frame(width: 270, height: 100)
+                    .overlay(
+                        Text("Continue").font(Font.custom("HappyMonkey-Regular", size: 37 )).foregroundColor(.white).shadow(color: .white, radius: 1))
+                   
+            }
+            .frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.9, alignment: .bottomTrailing)
+//
+            
+        } //close zstack
+            
+        } //close geometry reader
         }
 }
 
