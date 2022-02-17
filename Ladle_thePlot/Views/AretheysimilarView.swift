6b781973bb7b43e424a32a51438168dd2b5cb122
Rosa Cimmino <rosa.cimmino.1997@gmail.com>
2022-02-17 12:16:54 +0100

    DRAG AND DROP AFAMMOCK

---
+++ Ladle_thePlot/Views/AretheysimilarView.swift
@@ -0,0 +1,22 @@
+//
+//  AretheysimilarView.swift
+//  Ladle_thePlot
+//
+//  Created by Rosa Cimmino on 17/02/22.
+//
+
+import SwiftUI
+
+struct AretheysimilarView: View {
+    var body: some View {
+        Text("iew to see if they are simila")
+        
+        
+    }
+}
+
+struct AretheysimilarView_Previews: PreviewProvider {
+    static var previews: some View {
+        AretheysimilarView()
+    }
+}
--- Ladle_thePlot/Views/CreateRecipeView.swift
+++ Ladle_thePlot/Views/CreateRecipeView.swift
@@ -10,16 +10,17 @@ import SwiftUI
 
 
 struct CreateRecipeView: View {
-    
+    @State var isDragged : Bool = false
     @State var dragAmount = CGSize.zero
     
     var body: some View {
         
         GeometryReader { geometry in
-            HStack{
+//            HStack{
 //                The space for the timeline
 //                Rectangle().frame(width: 120, height: 400)
-                Text("The space for the timeline!!!").frame(width: 120, height: 400)
+//                Text("The space for the timeline!!!").frame(width: 120, height: 400)
+    
 //
         ZStack{
             RoundedRectangle(cornerRadius: 50, style: .continuous)
@@ -88,6 +89,14 @@ struct CreateRecipeView: View {
                 
             }.position(x: geometry.size.width/20, y: geometry.size.width/3)
             
+            
+            Image("blender")
+                    .resizable()
+                    .scaleEffect(0.5)
+                    .aspectRatio(1.2, contentMode: .fit)
+                    .position(x: geometry.size.width - geometry.size.width/4, y: geometry.size.height - geometry.size.height/4)
+            
+            
             VStack (spacing:2) {
                 
                 Text("Let's make it!")
@@ -100,25 +109,30 @@ struct CreateRecipeView: View {
 
                     Group{
                         ZStack{
-                        Circle()
+                            Circle()
                             Image("latte")
                                 .resizable()
                                 .scaledToFit()
-                                .scaleEffect(0.8)
+                                .scaleEffect(isDragged ? 0.4 : 0.8)
                                 .offset(dragAmount)
-                                .zIndex(dragAmount == .zero ? 1 : 0)
-//                            work on this one to drag on top of things
-//                            works only on the current zstack
-                            
-                            
+                                .zIndex(1)
+                                                       
 //                            drag gesture
                                 .gesture(DragGesture(coordinateSpace: .global)
                                             .onChanged{ self.dragAmount = CGSize(width : $0.translation.width, height: $0.translation.height)
                                 }
                                             .onEnded { _ in
-                                                      self.dragAmount = .zero
+                                    if abs(dragAmount.width) > geometry.size.width/3 {
+                                        isDragged = true
+                                        
+                                        
+//                                        remove the ingredient
+                                    } else {
+                                        self.dragAmount = .zero
+                                        isDragged = false
+                                    }
+                                  
                                 }
-
                                 )
 //
                         }
@@ -128,6 +142,7 @@ struct CreateRecipeView: View {
                                 .resizable()
                                 .scaledToFit()
                                 .scaleEffect(0.8)
+                               
                         }
                         
                         ZStack{
@@ -145,6 +160,7 @@ struct CreateRecipeView: View {
                                 .scaledToFit()
                                 .scaleEffect(0.8)
                         }
+                        
                     
                     
                     }
@@ -157,23 +173,18 @@ struct CreateRecipeView: View {
 //                avatar
                 
                 
-                HStack{
+                HStack (spacing: 50){
                 Image ("character_giorgia")
                         .resizable()
                         .scaleEffect(0.8)
                         .aspectRatio(0.7, contentMode: .fit)
 //                        .position(x: 250, y: 650)
                         .position(x: geometry.size.width/4.3, y: geometry.size.height/2.6)
+
+//                blender
                     
-//                    blender
-                    Circle()
-//                        .resizable()
-                        .scaleEffect(1.2)
-//                        .aspectRatio(0.7, contentMode: .fit)
-//                        .position(x: 250, y: 650)
-                        .position(x: geometry.size.width/4.3, y: geometry.size.height/3)
-                    
-                
+               
+
                 }
                 
             } //close the VSTack
@@ -181,7 +192,9 @@ struct CreateRecipeView: View {
         } //close the ZStack
         } //close geometry bracket
     }
-}
+    }
+
+    
 
 struct CreateRecipeView_Previews: PreviewProvider {
     static var previews: some View {
